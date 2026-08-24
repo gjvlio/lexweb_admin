07e74191b68afaa554396bd2ecc7bae2315f56dd
Karl Logdat <logdatk@gmail.com>
2026-08-24 11:58:36 +0800

    chore: added custom order sample rows

--- src/pages/Orders/OrdersData.js
+++ src/pages/Orders/OrdersData.js
@@ -20,6 +20,16 @@ export const oneTimeKeys = [
     { key: "status" },
 ]
 
+export const customKeys = [
+    { key: "id" },
+    { key: "client" },
+    { key: "vendor" },
+    { key: "productType" },
+    { key: "category" },
+    { key: "orderDate" },
+    { key: "status" },
+]
+
 export const subscriptionRows = [
     "ID", "Law Firm", "Plan", "Start Date", "Renewal Date", "Billing Cycle", "Price", "Status",
 ]
@@ -90,6 +100,31 @@ export const oneTimeOrders = [
         "client": "Bautista Law Office",
         "orderDate": new Date(2026, 6, 5),
         "price": 1000,
+        "status": "Paid",
+    },
+]
+
+export const customRows = [
+    "ID", "Client", "Vendor", "Type", "Category", "Order Date", "Status",
+]
+
+export const customOrders = [
+    {
+        "id": 100,
+        "client": "Bautista Law Office",
+        "vendor": "Bautista Law Office",
+        "productType": "Logo",
+        "category": "Scale",
+        "orderDate": new Date(2025, 5, 6),
+        "status": "Paid",
+    },
+    {
+        "id": 727,
+        "client": "Bautista Law Office",
+        "vendor": "Simple Law Firm",
+        "productType": "Copywrite",
+        "category": "Header",
+        "orderDate": new Date(2025, 9, 6),
         "status": "Paid",
     },
 ]
