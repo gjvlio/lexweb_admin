044b7804d455b19c76ae2db9f3031214cff37219
Karl Logdat <logdatk@gmail.com>
2026-08-24 11:46:57 +0800

    chore: added subscription and one time purchase column keys

--- src/pages/Orders/OrdersData.js
+++ src/pages/Orders/OrdersData.js
@@ -1,3 +1,25 @@
+export const subscriptionKeys = [
+    { key: "id" },
+    { key: "lawFirm" },
+    { key: "plan" },
+    { key: "startDate" },
+    { key: "renewalDate" },
+    { key: "billingCycle" },
+    { key: "price" },
+    { key: "status" },
+]
+
+export const oneTimeKeys = [
+    { key: "id" },
+    { key: "productName" },
+    { key: "productType" },
+    { key: "category" },
+    { key: "client" },
+    { key: "orderDate" },
+    { key: "price" },
+    { key: "status" },
+]
+
 export const subscriptionRows = [
     "ID", "Law Firm", "Plan", "Start Date", "Renewal Date", "Billing Cycle", "Price", "Status",
 ]
@@ -63,47 +85,7 @@ export const oneTimeOrders = [
     {
         "id": 100,
         "productName": "Hammer-Logo",
-        "type": "Logo",
-        "category": "Hammer",
-        "client": "Bautista Law Office",
-        "orderDate": new Date(2026, 6, 5),
-        "price": 1000,
-        "status": "Paid",
-    },
-    {
-        "id": 100,
-        "productName": "Hammer-Logo",
-        "type": "Logo",
-        "category": "Hammer",
-        "client": "Bautista Law Office",
-        "orderDate": new Date(2026, 6, 5),
-        "price": 1000,
-        "status": "Paid",
-    },
-    {
-        "id": 100,
-        "productName": "Hammer-Logo",
-        "type": "Logo",
-        "category": "Hammer",
-        "client": "Bautista Law Office",
-        "orderDate": new Date(2026, 6, 5),
-        "price": 1000,
-        "status": "Paid",
-    },
-    {
-        "id": 100,
-        "productName": "Hammer-Logo",
-        "type": "Logo",
-        "category": "Hammer",
-        "client": "Bautista Law Office",
-        "orderDate": new Date(2026, 6, 5),
-        "price": 1000,
-        "status": "Paid",
-    },
-    {
-        "id": 100,
-        "productName": "Hammer-Logo",
-        "type": "Logo",
+        "productType": "Logo",
         "category": "Hammer",
         "client": "Bautista Law Office",
         "orderDate": new Date(2026, 6, 5),
