c93dfc0b5a917094f25873d120261a21e005a74c
Karl Logdat <logdatk@gmail.com>
2026-08-26 14:03:38 +0800

    style: redesigned whole order page for site consistency

--- src/components/orders/TableTab.jsx
+++ src/components/orders/TableTab.jsx
@@ -1,5 +1,5 @@
 export default function TableTab({name, isActive = false, onClick  }) {
-  const mainClasses = "px-8 py-2 flex items-center justify-center border-r border-gray-500 last:border-none transition-colors"
+  const mainClasses = "px-8 py-2 flex items-center justify-center text-sm border-r border-slate-200 last:border-none transition-colors"
   const hoverClasses = "hover:bg-brand-purple hover:text-white"
   const activeClasses = "bg-brand-purple text-white"
 
