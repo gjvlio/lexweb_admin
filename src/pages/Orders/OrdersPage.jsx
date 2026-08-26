9d62d028cf51a134eb38a41b2d1bdee8e2bbfdd6
Karl Logdat <logdatk@gmail.com>
2026-08-26 16:21:48 +0800

    fix: removed visible x scroll on orders page

--- src/pages/Orders/OrdersPage.jsx
+++ src/pages/Orders/OrdersPage.jsx
@@ -222,7 +222,7 @@ export default function OrdersPage() {
   };
 
   return (
-    <div className="relative -left-8 w-[calc(100%+4rem)] -top-8 h-full bg-white text-gray-500">
+    <div className="-m-6 min-h-[calc(100vh-68px)] overflow-x-hidden bg-white text-gray-500">
       <div className="px-10 pt-[18px] pb-4 flex items-start justify-between gap-6">
         <span
           className="font-sans hover:underline cursor-pointer"
